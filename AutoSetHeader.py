df398fafcb0b8c8cb5305e63a53c6b7a6d038e66
test <62587920+tmp302@users.noreply.github.com>
2022-07-26 18:53:58 +0800

    Add files via upload

---
+++ AutoSetHeader.py
@@ -0,0 +1,24 @@
+rawHeader = '''GET /test HTTP/1.1
+Host: xxx.xxx.com
+Accept: application/json, text/plain, */*
+Sec-Ch-Ua-Mobile: ?0
+User-Agent: xxxxxx
+Origin: https://xxx.xxx.com
+Sec-Fetch-Site: same-site
+Sec-Fetch-Mode: cors
+Sec-Fetch-Dest: empty
+Referer: https://xxx.xxx.com/
+Accept-Encoding: gzip, deflate
+Accept-Language: zh-CN,zh;q=0.9
+Connection: close
+
+'''
+
+splitHeaderA = rawHeader.strip().split("\n")
+splitHeaderB = splitHeaderA[1:]
+HeaderInfo = {}
+for i in splitHeaderB:
+    listHeader = i.strip().split(":",1)
+    HeaderInfo[listHeader[0]] = listHeader[1].strip()
+
+print(HeaderInfo)
